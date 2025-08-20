6b0363f9fe6f091be4d9a06b25126e2ec410ea31
Davida CSIG <davida_csig@Davidas-iMac.local>
2025-08-20 17:39:15 +0300

    Update new model interface

--- src/app/interfaces/new-models.interface.ts
+++ src/app/interfaces/new-models.interface.ts
@@ -12,12 +12,51 @@ export interface Section {
   id: string;
   title: string;
   maxScore: number;
-  type: StdSection | DQSection;
+  type: "standard" | "dq";
+  questions: StdQuestion[] | DQQuestion[];
 }
 
 // SectionType
 
-//
-export interface StdSection {}
+export interface StdQuestion {
+  id: string;
+  subject: string;
+  level: number;
+  parent: string;
+  score: number;
+}
 
-export interface DQSection {}
+export interface DQQuestion {
+  id: string;
+  subsections: Subsection[];
+}
+
+export interface Subsection {
+  id: string;
+  instruction: string;
+  questions: SubQuestion[];
+}
+
+export interface SubQuestion {
+  quiz: string;
+  sq: SQ[];
+}
+export interface SQ {
+  id: string;
+  month: string;
+  answer: string;
+  elements: Element[];
+  evals: Eval[];
+}
+
+export interface Element {
+  id: string;
+  name: string;
+  data: number;
+}
+
+export interface Eval {
+  id: string;
+  name: string;
+  score: number;
+}
